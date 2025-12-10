c279730ae9547d43dfb1781cae18443831c35357
sagar.sachdev <sagar.sachdev@torusgroup.in>
2025-12-10 20:06:14 +0530

    Handle comma separated numbers in add()

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -1,10 +1,16 @@
 class StringCalculator {
   int add(String numbers) {
-    if (numbers.trim().isEmpty) {
+    final trimmed = numbers.trim();
+    if (trimmed.trim().isEmpty) {
       return 0;
     }
 
+    final parts = trimmed.split(',');
+
     // for now assume it's a single number input
-    return int.parse(numbers.trim());
+    return parts.map((part) => int.parse(part)).fold(
+          0,
+          (sum, value) => sum + value,
+        );
   }
 }
